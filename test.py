97851ed226e7028e56c7f2a227b839e5b93ffaef
Jaynath <jaynath120.kumar@gmail.com>
2018-05-06 12:37:39 +0530

    Pushed code to GIT

---
+++ test.py
@@ -0,0 +1,16 @@
+def sum(a, b):
+ print('Sum of number a : {} and b : {} is {}'.format(a,b,a+b))
+
+def sub(a, b):
+ print('Substraction of number a : {} and b : {} is {}'.format(a,b,a-b))
+
+def multiply(a, b):
+ print('Multiplication of number a : {} and b : {} is {}'.format(a,b,a*b))
+
+if __name__ == '__main__':
+ a = 30
+ b = 10
+ sum(a,b)
+ sub(a,b)
+ multiply(a,b)	
+
