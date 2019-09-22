e28f1e48855ab10aae82d420ca34e683a9cf7535
Jaynath1992 <jaynath120.kumar@gmail.com>
2019-09-22 20:56:48 +0530

    Update 8_Factorial.py

--- Python_Classes/Python_Day1_to_4/Python_Assignment_Exercise _1/8_Factorial.py
+++ Python_Classes/Python_Day1_to_4/Python_Assignment_Exercise _1/8_Factorial.py
@@ -12,3 +12,14 @@ def Calculate_Factorial(num):
 
 num = input('Enter number to get it\'s factorial : ')
 Calculate_Factorial(num)
+
+# REcursive function for factorial
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+  
+if __name__ == '__main__':
+    print factorial(5)
