9fb6d5118581bc96ac748c86d87a551628975f19
Jaynath1992 <jaynath120.kumar@gmail.com>
2025-02-15 15:55:41 +0530

    Create requirements_sh_learnings.py

---
+++ Pytest_Learnings/requirements_sh_learnings.py
@@ -0,0 +1,40 @@
+# install python 3.7.6+
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.7
+sudo apt-get --reinstall install python3-apt
+sudo apt-get --reinstall install apt-transport-https
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev
+sudo apt-get install python3-setuptools
+sudo pip3 install pyopenssl --upgrade
+sudo update-alternatives --config python3
+sudo apt install python3-pip
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
+sudo update-alternatives --config python3
+pip3 install --upgrade google-api-python-client
+sudo python3 -m pip install -U pip
+pip3 install pytest
+pip3 install termcolor
+pip3 install pytest-shell-utilities
+pip3 install pytest-loop
+pip3 install pluggy
+pip3 install pytest-html
+pip3 install pytest-email
+pip3 install kubernetes
+pip3 install pytest-subprocess
+pip3 install requests google google.auth \
+google-api-python-client google-cloud-iam
+pip3 install python-jwt
+pip3 install ecdsa pycryptodome
+pip3 install cryptography
+pip3 install python-jwt
+pip3 install oauth2client
+pip3 install pyOpenSSL
+pip3 install google-cloud-shell
+pip3 install pytest-repeat
+pip3 install pytest-loop
+pip3 install psycopg2-binary
+pip3 install pytest-xdist
+pip3 install pytest-slack
+pip3 install --upgrade requests
